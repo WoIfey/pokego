4cdcbfadff4c3a5a7ef30f5c31c8b80b8427205e
WoIfey <wuflerz@outlook.com>
2024-01-15 10:48:10 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ function App() {
   const [message, setMessage] = useState(null)
   const [isShiny, setIsShiny] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [notCaught, setNotCaught] = useState(false)
 
   const handleSearch = async () => {
     try {
-      const randomPokemon = Math.floor(Math.random() * 1017) + 1
       setLoading(true)
+      const randomPokemon = Math.floor(Math.random() * 1017) + 1
       const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
       const data = await res.json()
 
@@ -56,6 +57,7 @@ function App() {
           setMessage(`✅ You caught ${caughtPokemonName}!`)
           setPokemon(null)
           setIsShiny(false)
+          setNotCaught(false)
           return newCaughtPokemon
         })
         /*         return chance <= catched */
@@ -63,9 +65,12 @@ function App() {
         setMessage(`❌ Oh no! ${caughtPokemonName} ran away!`)
         setPokemon(null)
         setIsShiny(false)
+        setNotCaught(true)
+
         setTimeout(() => {
+          setNotCaught(false)
           handleSearch()
-        }, 3000)
+        }, 2000)
       }
     } else {
       setMessage("No Pokémon to catch. Search for a Pokémon first.")
@@ -82,19 +87,20 @@ function App() {
       }
     })
 
-    setMessage(`Caught Pokémon:\n${caughtPokemonNames}`)
+    setMessage(`Caught Pokémon: ${caughtPokemonNames}`)
   }
 
   return (
     <div className="flex flex-col justify-center items-center h-dvh bg-slate-100 dark:bg-[url(/pokego/bg.jpg)] text-white">
       <div className='bg-slate-700 min-w-max w-1/5 h-full flex justify-center items-center flex-col'>
+        <h1 className='text-2xl mb-2 font-bold tracking-widest'>Pokémon Catcher</h1>
         {message && (
-          <div className='bg-slate-800 w-full p-6 mb-4 flex justify-center items-center'>
+          <div className='bg-slate-800 w-full p-6 flex justify-center items-center'>
             <p className='max-w-80 max-h-32 overflow-y-auto'>{message}</p>
           </div>
         )}
         {pokemon && (
-          <div className='bg-slate-800 w-full p-6 mb-4'>
+          <div className='bg-slate-800 w-full p-6 border-y-2'>
             <div className='flex flex-col gap-2'>
               <div>
                 <div className='flex gap-2'>
@@ -113,24 +119,35 @@ function App() {
                   </div>
                 </div>
               </div>
-              <button onClick={handleCatch} className="bg-blue-600 text-white py-2 rounded hover:bg-blue-800 transition-all">Throw Pokéball</button>
+              <button onClick={handleCatch} className="bg-blue-600 text-white py-2 rounded hover:bg-blue-800 transition-all flex justify-center items-center gap-2">
+                <img src="/pokego/pokeball.png" alt="" className='size-5 mt-0.5' />
+                <p className='text-lg font-bold'>Throw Poké Ball</p>
+              </button>
             </div>
           </div>
         )}
-        <div className='flex gap-2'>
-          <div className='flex gap-2'>
-            <button type="submit" onClick={handleSearch} className="bg-blue-600 text-white px-4 py-2 h-10 rounded hover:bg-blue-800 transition-all flex justify-center items-center gap-1">
+        {(pokemon || !loading) && !notCaught && (
+          <div className='flex gap-2 mt-4'>
+            <button type="submit" onClick={handleSearch} className="bg-blue-600 text-white px-4 py-2 h-10 rounded hover:bg-blue-800 transition-all flex justify-center items-center gap-2">
               <img src="/pokego/search.svg" alt="" className='size-4' />
-              <p>Search</p>
+              <p className={loading ? 'opacity-0' : 'opacity-100'}>Search</p>
+              {loading && (
+                <div className="flex items-center justify-center absolute ml-5">
+                  <div className="animate-spin rounded-full h-6 w-6 border-t-4 border-white border-solid"></div>
+                </div>
+              )}
             </button>
-            {loading && (
-              <div className="flex items-center justify-center">
-                <div className="animate-spin rounded-full h-8 w-8 border-t-4 border-blue-500 border-solid"></div>
-              </div>
-            )}
+            <button onClick={showCaughtPokemon} className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition-all flex justify-center items-center gap-2">
+              <img src="/pokego/storage.png" alt="" className='size-5' />
+              <p>Pokémon Storage</p>
+            </button>
           </div>
-          <button onClick={showCaughtPokemon} className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition-all">Pokémon Storage</button>
-        </div>
+        )}
+        {notCaught && (
+          <div className="flex items-center justify-center mt-4">
+            <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-white border-solid"></div>
+          </div>
+        )}
       </div>
     </div>
   )
